28f4e663a73625181b2af6e24b2da6792daaf29f
saiki <127001sudais01@gmail.com>
2025-02-22 07:54:45 +0800

    square completed

---
+++ Unit_1/problem_wk2/WK_2_2_1.py
@@ -0,0 +1,9 @@
+"""
+Problem Wk.2.2.1: Square
+Define a procedure square(x) that takes a numeric parameter x and returns its square.
+It should have type num -> num.
+"""
+
+
+def square(x: int | float) -> int | float:
+    return x * x
