377f607648e231ef597c565708a964c64cd13b1a
saiki <127001sudais01@gmail.com>
2025-02-21 10:15:44 +0800

    initializing repository & python_tutorials

---
+++ Unit_1/problem_wk1/main.py
@@ -0,0 +1,37 @@
+"""
+print("<<============ WK.1.1.1 ============>>")
+# wk.1.1.1
+print(type(3.14))
+print(type(-34))
+print(type(True))
+print(type(None))
+print(type(3.0))
+"""
+
+"""
+print("<<============ WK.1.1.2 ============>>")
+# WK.1.1.2
+print(6 + 12 - 3)
+print(2.2 * 3.0)
+print(- - 4)
+print(10 / 3)
+print(10.0 / 3.0)
+print((2 + 3) * 4)
+print(2 + 3 * 4)
+print(2 ** 3 + 1)
+print(2.1 ** 2.0)
+"""
+
+print("<<============ WK.1.1.3 ============>>")
+print("3 > 4: ", 3 > 4)
+print("4.0 > 3.999 : ", 4.0 > 3.999)
+print("4 > 4 : ", 4 > 4)
+print("4 >= 4 : ", 4 >= 4)
+print("2 + 2 == 4 : ", 2 + 2 == 4)
+print("True or False : ", True or False)
+print("False or False : ", False or False)
+print("not False : ", not False)
+print("3.0 - 1.0 != 5.0 - 3.0 : ", 3.0 - 1.0 != 5.0 - 3.0)
+print("3 > 4 or (2 < 3 and 9 > 10) : ", 3 > 4 or (2 < 3 and 9 > 10))
+print("4 > 5 or 3 < 4 and 9 > 8 : ", 4 > 5 or 3 < 4 and 9 > 8)
+print("not (4 > 3 and 100 > 6) : ", not (4 > 3 and 100 > 6))
