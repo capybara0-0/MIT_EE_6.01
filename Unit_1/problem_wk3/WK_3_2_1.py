62b61a2c29be158ca0f4dc051f2540d0747add6b
saiki <127001sudais01@gmail.com>
2025-02-24 09:25:19 +0800

    Multiple times completed

---
+++ Unit_1/problem_wk3/WK_3_2_1.py
@@ -0,0 +1,16 @@
+"""
+Problem Wk.3.2.1: Multiple times
+Define a procedure multIA(m, n), which returns the product of `m` and `n`,
+assuming that `n` is a positive integer.
+Don't use * instead, use a while loop, and +.
+Your function should have type(num, positiveInt) -> num
+"""
+
+
+def multIA(m: float | int, n: int) -> float | int:
+    count = 1
+    result = 0
+    while count <= n:
+        result += m
+        count += 1
+    return result
